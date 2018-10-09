9729b9d0606ec7410e755fbb0fcf5984b5e80032
Gustave F. Gerhardt <gus@thewirecutter.com>
2018-10-09 13:24:39 -0500

    Create the meme

--- make-meme.js
+++ make-meme.js
@@ -25,13 +25,42 @@ const chooseMemeBase = () => {
 			} else {
 				captionMeme( res.data.memes[answer-1] );
 			}
-			rl.close();
 		});
 	});
 };
 
-const captionMeme = (answer) => {
-	console.log(JSON.stringify(answer));
+const captionMeme = (meme) => {
+	rl.question('Please provide a caption (1 of 2):', (caption1) => {
+		if (caption1.length) {
+			rl.question('Please provide a caption (2 of 2, optional):', (caption2 = '') => {
+				generateMeme(meme, caption1, caption2);
+				rl.close();
+				console.log('Creating your content...');
+			});
+		} else {
+			rl.close();
+			goodbye();
+		}
+	});
 };
+
+const generateMeme = (meme, caption1, caption2) => {
+	fetch(
+		'https://api.imgflip.com/caption_image?template_id=' + meme.id
+		+ '&text0=' + caption1
+		+ '&text1=' + caption2
+		+ '&username=dankestofmemes2000'
+		+ '&password=bR[DVqvjPGd87rAUVR8XvBzkNK@viC8W', {
+			method: "POST"
+		})
+		.then(res => { return res.json(); })
+		.then(r => {
+			return r.data.url;
+		})
+		.then(url => opn(url))
+		.then(() => process.exit())
+		.catch(console.error);
+};
+
 
 chooseMemeBase();
