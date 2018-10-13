02e3c3607bfdad19da4f77deb68564798bdaac4b
Justin Tucker <justin@certainstrings.com>
2018-10-12 19:02:03 -0700

    Set up file

---
+++ yo-dawg.js
@@ -0,0 +1,8 @@
+const readline = require('readline');
+const fetch = require('node-fetch');
+const opn = require('opn');
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
