0c0b2d9c893120b8ee964e2171c73856792a345c
Andrew Swaim <AndroidonEarth@users.noreply.github.com>
2019-10-02 19:28:37 -0700

    updated opn to open

--- reddit.js
+++ reddit.js
@@ -1,5 +1,5 @@
 const https = require('https');
-const opn = require('opn');
+const open = require('open');
 
 let numberOfMemes = 5;
 let filterNSFW = false;
@@ -11,7 +11,7 @@ function createMemeList(body) {
     if (body.data.children[i].data.preview.images[0].source.url) {
       // Basically doesn't display the image only if the filter is turned on AND the post is nsfw
       if (filterNSFW == false && body.data.children[i].data.over_18 == false) {
-        opn(body.data.children[i].data.preview.images[0].source.url);
+        open(body.data.children[i].data.preview.images[0].source.url);
       }
     }
   }
